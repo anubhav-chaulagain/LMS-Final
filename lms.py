d04ea5d8671db5b266eb1c5ef27ca70b60153865
GAURAB <gaurabkhadka4082@gmail.com>
2024-03-01 20:02:53 +0545

    final product

--- lms.py
+++ lms.py
@@ -4,21 +4,17 @@ from PIL import Image, ImageTk
 import sqlite3
 from tkinter import messagebox
 
-# GUI configurations
 win = Tk()
 win.config(bg='white')
 win.title('Login')
 win.geometry('900x500')
 win.resizable(False, False)
-win.config(bg='#ffe9cf')
 win.iconbitmap('icons/register_login_signup_icon_219991.ico')
 
-# Importing main application
 def lib():
     import liv
     
 
-# Function to registered the new account 
 def signingIn():
     conn = sqlite3.connect('registration.db')
     c = conn.cursor()
@@ -34,27 +30,32 @@ def signingIn():
     conn.commit()
     conn.close()
 
-    if (len(username.get()) == 0):
-        messagebox.showerror('Sign Up Error', 'Username must not be empty')
 
-    else:
-        if str(username.get()).lower()=='username' :
-            messagebox.showerror('Login Error', 'Given credentials can not be username')
+    if str(username.get()).lower()=='username' :
+        messagebox.showerror('Login Error', 'Given credentials can not be username')
 
-        elif str(password.get()).lower()=='password':
-             messagebox.showerror('Login Error', 'Given credentials can not be password')
+    elif str(password.get()).lower()=='password':
+         messagebox.showerror('Login Error', 'Given credentials can not be password')
 
-        elif len(str(password.get()))<=8:
-            messagebox.showerror('Login Error', 'Password should be greater than 8 characters')
+    elif len(str(password.get()))<=8:
+        messagebox.showerror('Login Error', 'Password should be greater than 8 characters')
 
-        elif valid_user:
-            messagebox.showerror('Login Error', 'Username already exists')
+    elif valid_user:
+        messagebox.showerror('Login Error', 'Username already exists')
 
     
-        else:
-            conn = sqlite3.connect('registration.db')
-            c = conn.cursor()
+    else:
+        conn = sqlite3.connect('registration.db')
+        c = conn.cursor()
 
+    #     c.execute(
+    #         '''
+    #     CREATE TABLE registration_table(
+    #     username text,
+    #     password text
+    #     )
+    # '''
+    # )
         
         c.execute(
             'INSERT INTO registration_table VALUES (:name, :pass)',
@@ -68,7 +69,7 @@ def signingIn():
         conn.commit()
         conn.close()
 
-# Function to connect to database and validate the account
+
 def logingIn():
     conn = sqlite3.connect('registration.db')
     c = conn.cursor()
@@ -94,20 +95,20 @@ def logingIn():
 
     conn.commit()
     conn.close()
- 
+
 
 # Log In Image and frame
 
-log_img = CTkImage(light_image=Image.open('Images/login1.jpg'), size=(500, 327))
+log_img = CTkImage(light_image=Image.open('Images/login.png'), size=(500, 327))
 log_label = CTkLabel(win,text='', image=log_img).place(x=50, y=70)
 
 
-# Log In ------------------------------------------------------------------------------------------------------------------
+# Log In 
 
 def logIn():
 
     lm_frame = Frame(win, width=275, height=280, border=0)
-    lm_frame.config(background='#ffe9cf')
+    lm_frame.config(background='white')
     lm_frame.place(x=600, y=100)
 
     log_label = CTkLabel(lm_frame, text='Log In', text_color='#57a1f8', font=('Microsoft YaHei UI Light', 23, 'bold')).place(relx=.4, rely=.05)
@@ -125,53 +126,54 @@ def logIn():
     l_btn = CTkButton(lm_frame, text='Log In', width=175, text_color='white',corner_radius=32, hover_color='#1d2a2b',command=logingIn)
     l_btn.place(x=70 , y=160)
 
-    l_dyhaa = CTkLabel(lm_frame, text='Create a new account !', text_color='black', font=('Microsoft YaHei UI Light', 13))
-    l_dyhaa.place(x=40, y=202)
+    l_dyhaa = CTkLabel(lm_frame, text='Create a new account', text_color='black', font=('Microsoft YaHei UI Light', 12))
+    l_dyhaa.place(x=50, y=200)
 
     # Create a transparent image
     transparent_image = Image.new('RGBA', (1, 1), (0, 0, 0, 0))
     transparent_photo = ImageTk.PhotoImage(transparent_image)
 
-    sign_up_btn = Button(lm_frame, text='Sign Up', foreground='#57a1f8', font=('Microsoft YaHei UI Light', 11, 'bold'), background='#ffe9cf', border=0, cursor='hand2', command=signIn)
+    sign_up_btn = Button(lm_frame, text='Sign In', foreground='#57a1f8', font=('Microsoft YaHei UI Light', 11, 'bold'), background='white', border=0, cursor='hand2', command=signIn)
     sign_up_btn.place(x=180, y=200)
 
 
 
+# Log In ------------------------------------------------------------------------------------------------------------------
 
-# Sign up ----------------------------------------------------------------------------------------------------------------
+
+# Sign Up 
     
 def signIn():
     m_frame = Frame(win, width=275, height=280, border=0)
-    m_frame.config(background='#ffe9cf')
+    m_frame.config(background='white')
     m_frame.place(x=600, y=100)
 
-    sign_label = CTkLabel(m_frame, text='Sign Up', text_color='#57a1f8', font=('Microsoft YaHei UI Light', 23, 'bold')).place(relx=.4, rely=.05)
+    sign_label = CTkLabel(m_frame, text='Sign In', text_color='#57a1f8', font=('Microsoft YaHei UI Light', 23, 'bold')).place(relx=.4, rely=.05)
 
     global username
     global password
 
     username = CTkEntry(m_frame, placeholder_text='Username', width=175, font=('Microsoft YaHei UI Light', 16), border_width=0)
     username.place(x=70, y=70)
-    
 
     password = CTkEntry(m_frame, placeholder_text='Password', width=175, font=('Microsoft YaHei UI Light', 16), border_width=0)
     password.place(x=70, y=110)
 
 
-    btn = CTkButton(m_frame, text='Sign Up', width=175, text_color='white',corner_radius=32, hover_color='#1d2a2b', command=signingIn)
+    btn = CTkButton(m_frame, text='Sign In', width=175, text_color='white',corner_radius=32, hover_color='#1d2a2b', command=signingIn)
     btn.place(x=70 , y=160)
 
-    dyhaa = CTkLabel(m_frame, text='Already have an account?', text_color='black', font=('Microsoft YaHei UI Light', 13))
-    dyhaa.place(x=20, y=202)
+    dyhaa = CTkLabel(m_frame, text='Already have an account?', text_color='black', font=('Microsoft YaHei UI Light', 11))
+    dyhaa.place(x=50, y=200)
 
     # Create a transparent image
     transparent_image = Image.new('RGBA', (1, 1), (0, 0, 0, 0))
     transparent_photo = ImageTk.PhotoImage(transparent_image)
 
-    login_btn = Button(m_frame, text='Log In', foreground='#57a1f8', font=('Microsoft YaHei UI Light', 11, 'bold'), background='#ffe9cf', border=0, cursor='hand2', command=logIn)
+    login_btn = Button(m_frame, text='Log In', foreground='#57a1f8', font=('Microsoft YaHei UI Light', 11, 'bold'), background='white', border=0, cursor='hand2', command=logIn)
     login_btn.place(x=180, y=200)
 
-
+# Sign up ----------------------------------------------------------------------------------------------------------------
 signIn()
 
 
